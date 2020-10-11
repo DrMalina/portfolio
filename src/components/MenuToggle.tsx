65ba13e14f73bacd41288d8f184020b54d78a6f4
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-10-12 00:41:45 +0200

    Change MenuToggle into separate component

--- src/components/MenuToggle.tsx
+++ src/components/MenuToggle.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
-import { IconMenu } from './IconMenu';
 
-type MenuToggleProps = {
+interface IconMenuProps {
   isMenuOpen: boolean;
+}
+
+function IconMenu({ isMenuOpen }: IconMenuProps) {
+  if (isMenuOpen) {
+    // show close icon
+    return (
+      <svg
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        className="fill-current w-8 h-8"
+        aria-hidden="true"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="{2}"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+    );
+  }
+  // show burger menu icon
+  return (
+    <svg
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className="fill-current w-8 h-8"
+      aria-hidden="true"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="{2}"
+        d="M4 6h16M4 12h16M4 18h16"
+      />
+    </svg>
+  );
+}
+interface MenuToggleProps extends IconMenuProps {
   setIsMenuOpen: () => void;
-};
+}
 
 export function MenuToggle({ isMenuOpen, setIsMenuOpen }: MenuToggleProps) {
   return (
