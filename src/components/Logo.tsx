e29c007caf2f96596e9cd9993660361b59800d26
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-10-12 00:42:15 +0200

    Change Logo to be a separate component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function Logo() {
   return (
-    <div aria-label="Logo: Maciej Malinowski" className="-mt-2 font-bold text-3xl text-gray-900">
+    <div role="img" aria-label="" className="-mt-2 font-bold text-3xl text-gray-900">
       <span aria-hidden="true">
         &lt;mm<span className="text-primary-500">/</span>&gt;
       </span>
