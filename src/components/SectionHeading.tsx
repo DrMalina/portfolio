5db7c089f4f779db0a7715b1c56c102aecbeca44
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-10-13 01:46:59 +0200

    Refactor SectionHeading to receive maxWidth and align props for centering sections

--- src/components/SectionHeading.tsx
+++ src/components/SectionHeading.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import cx from 'classnames';
 
 type SectionHeadingProps = {
   heading: string;
+  maxWidth?: 'sm' | 'md' | 'lg';
+  align?: 'left' | 'center';
 };
 
-export function SectionHeading({ heading }: SectionHeadingProps) {
+export function SectionHeading({ heading, maxWidth = 'sm', align = 'left' }: SectionHeadingProps) {
+  const sectionClassName = cx(
+    {
+      'max-w-sm': maxWidth === 'sm',
+      'max-w-md': maxWidth === 'md',
+      'max-w-lg': maxWidth === 'lg',
+      'md:ml-0': align === 'left',
+      'md:ml-auto': align === 'center',
+    },
+    'flex items-center mx-auto',
+  );
+
   return (
-    <div className="flex items-center max-w-sm md:ml-0 mx-auto">
+    <div className={sectionClassName}>
       <h2 className="text-3xl text-gray-800 font-bold whitespace-no-wrap">{heading}</h2>
       <span
         role="img"
