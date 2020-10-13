2bb1bda897b217037ab3dbad9e83a1dc04e6efc2
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-10-13 23:07:16 +0200

    Change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,7 +78,7 @@ module.exports = {
         background_color: `#f7fafc`,
         theme_color: `#8B40FC`,
         display: `minimal-ui`,
-        icon: `src/assets/favicon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/favicon.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
